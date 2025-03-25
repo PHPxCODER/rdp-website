57d59c99f590155ee5dbb8b45d6283cfc02bdfcb
Subhadeep <codevizag@gmail.com>
2025-03-25 13:25:02 +0530

    Auth UI Changes ✨

--- src/app/(landing)/auth/SigninForm.tsx
+++ src/app/(landing)/auth/SigninForm.tsx
@@ -258,6 +258,17 @@ export function SigninForm() {
                 {isSubmitting ? "Sending email..." : "Send OTP"}
               </Button>
 
+              <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
               <Button onPress={handleGoogleSignIn} isLoading={isGoogleLoading} disabled={isGoogleLoading} variant="light" radius="full" className="w-full" type="button">
                 {!isGoogleLoading && <GoogleIcon />}
                 Signin with Google
--- src/app/(landing)/auth/signup/SignupForm.tsx
+++ src/app/(landing)/auth/signup/SignupForm.tsx
@@ -110,6 +110,18 @@ export function SignupForm() {
             <Button type="submit" color="primary" radius="full" className="w-full" disabled={isSubmitting} isLoading={isSubmitting} onPress={handleCreateAccount}>
               {isSubmitting ? "Processing..." : "Create an account"}
             </Button>
+
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            
             <Button onPress={handleGoogleSignIn} isLoading={isGoogleLoading} disabled={isGoogleLoading} variant="light" radius="full" className="flex items-center gap-2">
               {!isGoogleLoading && <GoogleIcon />}
               Sign up with Google
